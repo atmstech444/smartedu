437544048b67d1a700e64b4c8944d227d00f26d7
Luka Koridze <lukakoridze13@gmail.com>
2023-11-07 14:35:38 +0400

    Update register-form.tsx

--- src/form/register-form.tsx
+++ src/form/register-form.tsx
@@ -10,51 +10,42 @@ const RegisterForm = () => {
   const [showPass, setShowPass] = useState(false);
   const [showConPass, setShowConPass] = useState(false);
   // use formik
-  const { handleChange, handleSubmit, handleBlur, errors, values, touched } =
-    useFormik({
-      initialValues: {
-        name: "",
-        email: "",
-        password: "",
-        passwordConfirmation: "",
-      },
-      validationSchema: register_schema,
-      onSubmit: (values, { resetForm }) => {
-        resetForm();
-      },
-    });
+  const { handleChange, handleSubmit, handleBlur, errors, values, touched } = useFormik({
+    initialValues: {
+      name: "",
+      surname: "",
+      email: "",
+      password: "",
+      passwordConfirmation: "",
+    },
+    validationSchema: register_schema,
+    onSubmit: (values, { resetForm }) => {
+      resetForm();
+    },
+  });
   return (
     <form onSubmit={handleSubmit}>
       <div className="sign__input-wrapper">
-        <h5>სახელი გვარი</h5>
+        <h5>სახელი</h5>
         <div className="sign__input mb-25">
-          <input
-            name="name"
-            value={values.name}
-            onChange={handleChange}
-            onBlur={handleBlur}
-            type="text"
-            placeholder="სახელი გვარი"
-            id="name"
-            required
-          />
+          <input name="name" value={values.name} onChange={handleChange} onBlur={handleBlur} type="text" placeholder="სახელი" id="name" required />
           <i className="fal fa-user"></i>
           {touched.name && <ErrorMsg error={errors.name} />}
         </div>
 
         <div className="sign__input-wrapper mb-25">
+          <h5>გვარი</h5>
+          <div className="sign__input">
+            <input name="surname" value={values.surname} onChange={handleChange} onBlur={handleBlur} type="surname" placeholder="გვარი" id="surname" required />
+            <i className="fal fa-user"></i>
+          </div>
+          {touched.email && <ErrorMsg error={errors.surname} />}
+        </div>
+
+        <div className="sign__input-wrapper mb-25">
           <h5>ელ. ფოსტა</h5>
           <div className="sign__input">
-            <input
-              name="email"
-              value={values.email}
-              onChange={handleChange}
-              onBlur={handleBlur}
-              type="email"
-              placeholder="ელ. ფოსტა"
-              id="email"
-              required
-            />
+            <input name="email" value={values.email} onChange={handleChange} onBlur={handleBlur} type="email" placeholder="ელ. ფოსტა" id="email" required />
             <i className="fal fa-envelope"></i>
           </div>
           {touched.email && <ErrorMsg error={errors.email} />}
@@ -63,16 +54,7 @@ const RegisterForm = () => {
         <div className="sign__input-wrapper mb-25">
           <h5>პარლი</h5>
           <div className="sign__input">
-            <input
-              name="password"
-              value={values.password}
-              onChange={handleChange}
-              onBlur={handleBlur}
-              type={showPass ? "text" : "password"}
-              placeholder="პაროლი"
-              id="password"
-              required
-            />
+            <input name="password" value={values.password} onChange={handleChange} onBlur={handleBlur} type={showPass ? "text" : "password"} placeholder="პაროლი" id="password" required />
             <i className="fal fa-lock"></i>
           </div>
           {touched.password && <ErrorMsg error={errors.password} />}
@@ -81,21 +63,10 @@ const RegisterForm = () => {
         <div className="sign__input-wrapper mb-10">
           <h5>გაიმეორე პაროლი</h5>
           <div className="sign__input">
-            <input
-              name="passwordConfirmation"
-              value={values.passwordConfirmation}
-              onChange={handleChange}
-              onBlur={handleBlur}
-              type={showConPass ? "text" : "password"}
-              placeholder="გაიმეორე პაროლი"
-              id="passwordConfirmation"
-              required
-            />
+            <input name="passwordConfirmation" value={values.passwordConfirmation} onChange={handleChange} onBlur={handleBlur} type={showConPass ? "text" : "password"} placeholder="გაიმეორე პაროლი" id="passwordConfirmation" required />
             <i className="fal fa-lock"></i>
           </div>
-          {touched.passwordConfirmation && (
-            <ErrorMsg error={errors.passwordConfirmation} />
-          )}
+          {touched.passwordConfirmation && <ErrorMsg error={errors.passwordConfirmation} />}
         </div>
       </div>
       <div className="sign__action d-flex justify-content-between mb-30">
