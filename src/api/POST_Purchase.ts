5a1efd0063333ebc2fc065d3f16864cec93d1321
Tusho7 <sandrotushurashvili7@gmail.com>
2023-12-20 15:45:25 +0400

    feat: create payment route to navigate to bog page

--- src/api/POST_Purchase.ts
+++ src/api/POST_Purchase.ts
@@ -2,29 +2,37 @@ import axios from "axios";
 import { API_PATH } from "./API_PATH";
 import { toast } from "react-toastify";
 
-export interface POST_Login_Params {
+export interface POST_Purchase_Params {
   token: string;
   course_id: number;
 }
 
-export async function POST_Purchase(data: POST_Login_Params) {
+export async function POST_Purchase(data: POST_Purchase_Params) {
   const config = {
     headers: {
       Authorization: `Bearer ${data.token}`,
     },
   };
-  try {
-    const response = await axios.post(API_PATH + "courses/" + data.course_id + "/purchase", null, config);
 
+  try {
+    const response = await axios.post(API_PATH + "purchases/bog/create-order/" + data.course_id, null, config);
+    console.log(response.data)
+    const paymentHref = response.data.links.redirect.href;
+    window.location.href = paymentHref;
     toast.success("კურსი ნაყიდია", {
       position: toast.POSITION.TOP_RIGHT,
       autoClose: 2000,
     });
+
     return response.data;
-  } catch (error: any) {
+  } catch (error) {
+    console.error("Error during purchase:", error);
+
     toast.error("დაფიქსირდა შეცდომა", {
       position: toast.POSITION.TOP_RIGHT,
       autoClose: 2000,
     });
+
+    throw error;
   }
 }
