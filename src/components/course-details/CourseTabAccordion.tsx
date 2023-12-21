aeb90ac606b7d8f9b151a811c0819c4c4a61f0ae
anrikopaliani <anrikopa@gmail.com>
2023-12-21 12:02:35 +0400

    fix: key errors and remove babelrc

--- src/components/course-details/CourseTabAccordion.tsx
+++ src/components/course-details/CourseTabAccordion.tsx
@@ -1,12 +1,13 @@
 import { I_Course_Details } from "@/api/GET_CourseDetails";
 import React from "react";
+import { v4 as uuidv4 } from "uuid";
 
 const CourseTabAccordion = ({ course }: { course: I_Course_Details }) => {
   return (
     <div className="course__curriculum">
       {course.syllabus.map((item, index) => {
         return (
-          <div key={`acc-${item.course_id}`} className="accordion" id="course__accordion">
+          <div key={`acc-${uuidv4()}`} className="accordion" id="course__accordion">
             <div className="accordion-item mb-50">
               <h2 className="accordion-header" id={`week-${index}`}>
                 <button className="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target={`#week-${index}-content`} aria-expanded="true" aria-controls={`week-${index}-content`}>
@@ -17,7 +18,7 @@ const CourseTabAccordion = ({ course }: { course: I_Course_Details }) => {
                 <div className="accordion-body">
                   {item.descriptions.map((description, index) => {
                     return (
-                      <div key={`des-${description.course_syllabus_id}`} className="course__curriculum-content d-sm-flex justify-content-between align-items-center">
+                      <div key={`des-${uuidv4()}`} className="course__curriculum-content d-sm-flex justify-content-between align-items-center">
                         <div className="course__curriculum-info">
                           <h3>
                             <span>{description.description}</span>
