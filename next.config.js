f3c6f9ad58686dacf8e7b10dd6f2acfbd9295876
anrikopaliani <anrikopa@gmail.com>
2023-12-21 14:59:16 +0400

    chore: change next config

--- next.config.js
+++ next.config.js
@@ -1,6 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: "export const dynamicParams = false",
+  reactStrictMode: true,
+  swcMinify: true,
   images: { unoptimized: true },
   compiler: {
     styledComponents: true,
