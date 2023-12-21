59bedbe8f55fd658e6b96689773c866a686d85d9
anrikopaliani <anrikopa@gmail.com>
2023-12-21 13:55:44 +0400

    chore: registry from nextjs

--- src/app/registry.tsx
+++ src/app/registry.tsx
@@ -1,3 +1,25 @@
+// "use client";
+
+// import React, { useState } from "react";
+// import { useServerInsertedHTML } from "next/navigation";
+// import { ServerStyleSheet, StyleSheetManager } from "styled-components";
+
+// export default function StyledComponentsRegistry({ children }: { children: React.ReactNode }) {
+//   // Only create stylesheet once with lazy initial state
+//   // x-ref: https://reactjs.org/docs/hooks-reference.html#lazy-initial-state
+//   const [styledComponentsStyleSheet] = useState(() => new ServerStyleSheet());
+
+//   useServerInsertedHTML(() => {
+//     const styles = styledComponentsStyleSheet.getStyleElement();
+//     styledComponentsStyleSheet.instance.clearTag();
+//     return <React.Fragment>{styles}</React.Fragment>;
+//   });
+
+//   if (typeof window !== "undefined") return <React.Fragment>{children as React.ReactNode}</React.Fragment>;
+
+//   return <StyleSheetManager sheet={styledComponentsStyleSheet.instance}>{children}</StyleSheetManager>;
+// }
+
 "use client";
 
 import React, { useState } from "react";
@@ -12,10 +34,10 @@ export default function StyledComponentsRegistry({ children }: { children: React
   useServerInsertedHTML(() => {
     const styles = styledComponentsStyleSheet.getStyleElement();
     styledComponentsStyleSheet.instance.clearTag();
-    return <React.Fragment>{styles}</React.Fragment>;
+    return <>{styles}</>;
   });
 
-  if (typeof window !== "undefined") return <React.Fragment>{children as React.ReactNode}</React.Fragment>;
+  if (typeof window !== "undefined") return <>{children}</>;
 
   return <StyleSheetManager sheet={styledComponentsStyleSheet.instance}>{children}</StyleSheetManager>;
 }
