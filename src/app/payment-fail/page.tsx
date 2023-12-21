1a99a9e5871d0f6c7341160c55eb67011847fffe
anrikopaliani <anrikopa@gmail.com>
2023-12-21 13:41:18 +0400

    chore: comment component style

--- src/app/payment-fail/page.tsx
+++ src/app/payment-fail/page.tsx
@@ -3,39 +3,40 @@ import Wrapper from "@/layout/DefaultWrapper";
 import React from "react";
 import Image from "next/image";
 import Error from "@/../public/assets/img/payment/error.png";
-import styled from "styled-components";
+// import styled from "styled-components";
 
 const Payment = () => {
   return (
     <div>
       <Wrapper>
-        <Container>
+        <h1>გადახდა ვერ განხორციელდა</h1>
+        {/* <Container>
           <Image src={Error} alt="Failed" />
           <Title>გადახდა ვერ განხორციელდა</Title>
-        </Container>
+        </Container> */}
       </Wrapper>
     </div>
   );
 };
 
-const Container = styled.div`
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  align-items: center;
-  gap: 33px;
+// const Container = styled.div`
+//   display: flex;
+//   flex-direction: column;
+//   justify-content: center;
+//   align-items: center;
+//   gap: 33px;
 
-  padding-bottom: 100px;
-  padding-top: 300px;
-`;
+//   padding-bottom: 100px;
+//   padding-top: 300px;
+// `;
 
-const Title = styled.p`
-  color: #000;
-  font-family: BPG Arial;
-  font-size: 40px;
-  font-style: normal;
-  font-weight: 400;
-  line-height: normal;
-`;
+// const Title = styled.p`
+//   color: #000;
+//   font-family: BPG Arial;
+//   font-size: 40px;
+//   font-style: normal;
+//   font-weight: 400;
+//   line-height: normal;
+// `;
 
 export default Payment;
