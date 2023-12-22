253308d5f1980201f72f69a845e3a5c6834ac3b0
anrikopaliani <anrikopa@gmail.com>
2023-12-22 16:40:07 +0400

    feat:seperate syllabis items into component

--- src/components/course-details/CourseTabAccordion.tsx
+++ src/components/course-details/CourseTabAccordion.tsx
@@ -1,40 +1,13 @@
 import { I_Course_Details } from "@/api/GET_CourseDetails";
 import React, { useState } from "react";
 import { v4 as uuidv4 } from "uuid";
+import Tab from "./Tab";
 
 const CourseTabAccordion = ({ course }: { course: I_Course_Details }) => {
-  const [isOpen, setIsOpen] = useState(false);
   return (
     <div className="course__curriculum">
       {course.syllabus.map((item, index) => {
-        return (
-          <div key={`acc-${uuidv4()}`} className="accordion" id="course__accordion">
-            <div className="accordion-item mb-50" onClick={() => setIsOpen(true)}>
-              <h2 className="accordion-header" id={`week-${index}`}>
-                <button className="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target={`#week-${index}-content`} aria-expanded="true" aria-controls={`week-${index}-content`}>
-                  {item.title}
-                </button>
-              </h2>
-              {isOpen && (
-                <div id={`week-${index}-content`} className="accordion-collapse collapse show" aria-labelledby={`week-${index}`} data-bs-parent="#course__accordion">
-                  <div className="accordion-body">
-                    {item.descriptions.map((description, index) => {
-                      return (
-                        <div key={`des-${uuidv4()}`} className="course__curriculum-content d-sm-flex justify-content-between align-items-center">
-                          <div className="course__curriculum-info">
-                            <h3>
-                              <span>{description.description}</span>
-                            </h3>
-                          </div>
-                        </div>
-                      );
-                    })}
-                  </div>
-                </div>
-              )}
-            </div>
-          </div>
-        );
+        return <Tab key={`acc-${uuidv4()}`} index={index} item={item} />;
       })}
     </div>
   );
