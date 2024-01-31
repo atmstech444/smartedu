434dd878f49c00f9d0c5c50def1da4ce1ea15b31
Tusho7 <sandrotushurashvili7@gmail.com>
2024-01-31 14:46:52 +0400

    feat: Create components for uploads and also create Tabs component to switch

--- src/app/admin/main/[id]/page.tsx
+++ src/app/admin/main/[id]/page.tsx
@@ -2,10 +2,15 @@
 import SecondNavbar from "../components/SecondNavbar";
 import { useParams } from "next/navigation";
 import { useState, useEffect } from "react";
+import Tabs from "./components/Tabs";
+import QuizUpload from "./components/QuizUpload";
+import ReadingUpload from "./components/ReadingUpload";
+import VideoUpload from "./components/VideoUpload";
 
 const page = () => {
   const { id } = useParams();
   const [courseData, setCourseData] = useState(null);
+  const [activeTab, setActiveTab] = useState("წასაკითხი");
 
   useEffect(() => {
     const storedData = localStorage.getItem(`course_${id}`);
@@ -14,9 +19,24 @@ const page = () => {
       setCourseData(JSON.parse(storedData));
     }
   }, [id]);
+
+  let content = null;
+  if (activeTab === "წასაკითხი") {
+    content = <ReadingUpload />;
+  } else if (activeTab === "ვიდეო") {
+    content = <VideoUpload />;
+  } else if (activeTab === "ქვიზი") {
+    content = <QuizUpload />;
+  }
+
   return (
-    <div>
+    <div className="flex gap-8">
       <SecondNavbar courseData={courseData} />
+
+      <div className="flex flex-col gap-10  mt-11">
+        <Tabs activeTab={activeTab} setActiveTab={setActiveTab} />
+        {content}
+      </div>
     </div>
   );
 };
--- src/app/admin/main/components/SecondNavbar.tsx
+++ src/app/admin/main/components/SecondNavbar.tsx
@@ -24,7 +24,7 @@ const SecondNavbar = ({ courseData }: any) => {
   };
   return (
     <div className="w-64 mt-11 px-4 border-r-2 border-[#D9EBF4] mb-12 min-h-[calc(100vh-150px)] flex flex-col justify-between">
-      <div className="pt-5 flex flex-col gap-4">
+      <div className=" flex flex-col gap-4">
         <img src={`https://smarteducation.shop/smarteducation_backend/public/${courseData?.cover_image}`} className="rounded-2xl" />
         <p className="text-base text-black font-semibold">{courseData?.title}</p>
         <div className="w-full h-[1px] bg-[#D1D1D1]"></div>
