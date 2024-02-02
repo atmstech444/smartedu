db13dff04af006ffd916b0fa24f79aec836ab683
mariamdatuka <mariam.datukishvili.7@gmail.com>
2024-02-02 04:27:15 -0800

    style:quiz layout

--- src/app/(root)/lectureDetails/[id]/page.tsx
+++ src/app/(root)/lectureDetails/[id]/page.tsx
@@ -1,8 +1,28 @@
+"use client";
 import Wrapper from "@/layout/DefaultWrapper";
-import React from "react";
+import { useRouter } from "next/navigation";
+import { useState, useEffect } from "react";
 import LectureNav from "../components/LectureNav";
 
 const page = ({ params }: { params: { id: number } }) => {
+  const [isDesktop, setIsDesktop] = useState(false);
+  const router = useRouter();
+  const currentPath = window.location.pathname;
+
+  const updateIsDesktop = () => {
+    setIsDesktop(window.innerWidth > 768);
+  };
+
+  useEffect(() => {
+    updateIsDesktop();
+    window.addEventListener("resize", updateIsDesktop);
+    return () => {
+      window.removeEventListener("resize", updateIsDesktop);
+    };
+  }, []);
+  if (isDesktop) {
+    router.push(currentPath + "/reading");
+  }
   return (
     <>
       <Wrapper>
--- src/app/(root)/lectureDetails/components/LectureNav.tsx
+++ src/app/(root)/lectureDetails/components/LectureNav.tsx
@@ -1,24 +1,51 @@
 "use client";
-import React from "react";
+
 import { useRouter } from "next/navigation";
 import { styled } from "styled-components";
 import Arrow from "../../../../public/assets/icons/arrowLeft.svg";
 import Image from "next/image";
+import Book from "@/public/assets/dynamic_icons/Book";
+import Video from "@/public/assets/dynamic_icons/Video";
+import Quiz from "@/public/assets/dynamic_icons/Quiz";
+
+const lectureData = [
+  {
+    title: "წასაკითხი მასალა",
+    icon: <Book />,
+  },
+  {
+    title: "ვიდეო:პროგრამირების საწყისები",
+    icon: <Video />,
+  },
+  {
+    title: "ვიდეო:პროგრამირების საწყისები",
+    icon: <Video />,
+  },
+  {
+    title: "ლექციის ბოლო:ქვიზი",
+    icon: <Quiz />,
+  },
+];
 
 const LectureNav = (id: any) => {
   const router = useRouter();
-
   const navigateToAboutPage = () => {
     router.push(`/watch/${id.id}/about`);
   };
 
   return (
     <div className="mt-[20%] p-[24px] md:mt-0 md:w-[30%] lg:w-[20%] bg-white rounded-md md:h-full">
-      <div>
+      <div className="flex flex-col">
         <Image onClick={() => router.push(`/profile`)} src={Arrow} width={24} height={24} alt="image" className="md:hidden mb-4" />
-        <CourseName></CourseName>
-        <AboutCourse onClick={navigateToAboutPage}>კურსის შესახებ</AboutCourse>
-        <p onClick={() => router.push(`/watch/${id.id}/course/1`)}>კურსი</p>
+        <LectureNumber>ლექცია 1</LectureNumber>
+        {lectureData.map((item, index) => (
+          <LectureMaterial>
+            <div className="flex gap-3 items-center">
+              {item.icon}
+              <p className="mb-0 text-dark">{item.title}</p>
+            </div>
+          </LectureMaterial>
+        ))}
       </div>
     </div>
   );
@@ -26,22 +53,23 @@ const LectureNav = (id: any) => {
 
 export default LectureNav;
 
-const AboutCourse = styled.button`
+const LectureMaterial = styled.button`
   color: #000;
   font-family: FiraGO;
-  font-size: 16px;
+  font-size: 14px;
   font-style: normal;
   font-weight: 500;
   line-height: 150%;
   cursor: pointer;
+  margin-bottom: 24px;
 `;
 
-const CourseName = styled.h1`
+const LectureNumber = styled.h1`
   margin-bottom: 32px;
   color: #000;
   font-family: FiraGO;
-  font-size: 16px;
+  font-size: 20px;
   font-style: normal;
-  font-weight: 500;
+  font-weight: 600;
   line-height: 150%;
 `;
