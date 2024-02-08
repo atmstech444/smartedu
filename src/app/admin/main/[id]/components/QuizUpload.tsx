ff33ad83919b1d5e425eb2be24642048471a10dc
Tusho7 <sandrotushurashvili7@gmail.com>
2024-02-08 11:49:18 +0400

    feat: add functionality to add more than one question with delete button

--- src/app/admin/main/[id]/components/QuizUpload.tsx
+++ src/app/admin/main/[id]/components/QuizUpload.tsx
@@ -1,41 +1,56 @@
-import React from "react";
+import React, { useState } from "react";
 import Image from "next/image";
+
 const QuizUpload = () => {
+  const [sections, setSections] = useState([{ id: 1 }]);
+
+  const handleAddItem = () => {
+    const newId = sections.length + 1;
+    setSections((prevSections) => [...prevSections, { id: newId }]);
+  };
+
+  const handleDeleteItem = (id: number) => {
+    setSections((prevSections) => prevSections.filter((section) => section.id !== id));
+  };
+
   return (
     <main>
-      <div className="border border-1-[#D1D1D1] p-4 rounded-lg w-[970px] h-auto flex flex-col gap-4">
-        <div className="flex gap-2">
-          <section className="relative">
-            <input
-              type="text"
-              name="question"
-              id="question"
-              className="w-[200px] h-[42px] resize-none rounded-lg pl-3 py-1 border border-1-[#D1D1D1] outline-none bg-transparent placeholder-[#000000] placeholder-opacity-60"
-              placeholder="ჩაწერე კითხვა"
-            />
-            <Image src="/assets/img/admin/pencil.png" className="absolute top-4 right-2" alt={""} width={12} height={12} />
-          </section>
+      {sections.map(({ id }, index) => (
+        <div key={id} className="border border-1-[#D1D1D1] p-4 rounded-lg w-[970px] h-auto flex flex-col gap-4 mt-5">
+          <div className="flex gap-2">
+            <section className="relative">
+              <input
+                type="text"
+                name="question"
+                id="question"
+                className="w-[200px] h-[42px] resize-none rounded-lg pl-3 py-1 border border-1-[#D1D1D1] outline-none bg-transparent placeholder-[#000000] placeholder-opacity-60"
+                placeholder="ჩაწერე კითხვა"
+              />
+              <Image src="/assets/img/admin/pencil.png" className="absolute top-4 right-2" alt={""} width={12} height={12} />
+            </section>
 
-          <div className="relative w-[220px] flex items-center gap-1">
-            <input id="type" placeholder="ლინკის ატვირთვა" className="border border-1-[#D1D1D1] outline-none w-44 rounded-lg p-2" onChange={() => {}} />
-            <Image src="/assets/img/admin/AddFile.png" width={16} height={16} alt={"Add Icon"} />
+            <div className="relative w-[220px] flex items-center gap-1">
+              <input id="type" placeholder="ლინკის ატვირთვა" className="border border-1-[#D1D1D1] outline-none w-44 rounded-lg p-2" onChange={() => {}} />
+              <Image src="/assets/img/admin/AddFile.png" width={16} height={16} alt={"Add Icon"} />
+            </div>
           </div>
-        </div>
 
-        <div className="flex gap-2 items-center relative">
-          <label className="flex gap-1">
-            <input type="radio" name="answer" id="answer" />
-          </label>
-          <input type="text" className="border border-1-[#D1D1D1] p-1 rounded-lg w-40 outline-none" placeholder="ჩაწერე პასუხი" />
-          <Image src="/assets/img/admin/pencil.png" className="absolute left-40" alt={""} width={12} height={12} />
+          <div className="flex gap-2 items-center relative">
+            <label className="flex gap-1">
+              <input type="radio" name="answer" id="answer" />
+            </label>
+            <input type="text" className="border border-1-[#D1D1D1] p-1 rounded-lg w-40 outline-none" placeholder="ჩაწერე პასუხი" />
+            <Image src="/assets/img/admin/pencil.png" className="absolute left-40" alt={""} width={12} height={12} />
+          </div>
+          {index !== 0 && (
+            <button onClick={() => handleDeleteItem(id)} className="text-white bg-[#2FA8FF] py-1 px-7 rounded-lg w-[200px] self-end">
+              წაშლა
+            </button>
+          )}
         </div>
+      ))}
 
-        <div>
-          <Image src="/assets/img/admin/plusicon.png" alt={""} width={20} height={20} className="cursor-pointer" />
-        </div>
-      </div>
-
-      <div className="w-5 h-5 ml-4 mt-4">
+      <div className="w-5 h-5 ml-4 mt-4" onClick={handleAddItem}>
         <Image src="/assets/img/admin/plusicon.png" alt={""} width={20} height={20} className="cursor-pointer w-full" />
       </div>
     </main>
