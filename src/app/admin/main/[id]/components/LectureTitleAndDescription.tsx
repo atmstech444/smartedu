7258911b847cea252c90efa292cf197366521145
Tusho7 <sandrotushurashvili7@gmail.com>
2024-02-13 12:04:02 +0400

    feat: create title and descption delete and create functions

--- src/app/admin/main/[id]/components/LectureTitleAndDescription.tsx
+++ src/app/admin/main/[id]/components/LectureTitleAndDescription.tsx
@@ -3,6 +3,9 @@ import Image from "next/image";
 import { addlectureTitleAndDescription } from "../services/addlectureTitleAndDescription";
 import { parseCookies } from "nookies";
 import Swal from "sweetalert2";
+import { getCourseById } from "../../services/getCoursesById";
+import { getLectureAndDescriptions } from "../services/getLectureAndDescriptions";
+import { deleteTitleAndDescription } from "../services/deleteTitleAndDescription";
 
 const useQueryParams = () => {
   const [lectureId, setLectureId] = useState<string | undefined | null>(undefined);
@@ -24,6 +27,7 @@ const LectureTitleAndDescription = () => {
   const [isTypingTextarea, setIsTypingTextarea] = useState(false);
   const [lectureTitle, setLectureTitle] = useState("");
   const [lectureDescription, setLectureDescription] = useState("");
+  const [titleDescriptionData, setTitleDescriptionData] = useState<{ id: number; title: string; description: string } | null>(null);
 
   const handleTypingInput = () => {
     setIsTypingInput(true);
@@ -52,7 +56,8 @@ const LectureTitleAndDescription = () => {
 
     try {
       const response = await addlectureTitleAndDescription(token, formData, lectureId);
-      if (response.message === "Lecture updated successfully") {
+      if (response.message === "lecture contet create successfully") {
+        setTitleDescriptionData(response.lecture_content);
         Swal.fire({
           icon: "success",
           title: response.message,
@@ -73,36 +78,113 @@ const LectureTitleAndDescription = () => {
     }
   };
 
-  return (
-    <div className="relative flex flex-col gap-4">
-      <div className="w-48 relative">
-        <input
-          type="text"
-          value={lectureTitle}
-          placeholder="ლექციის სათაური"
-          onChange={(e) => setLectureTitle(e.target.value)}
-          className="h-auto resize-none rounded-lg pl-3 py-1 border border-1-[#D1D1D1] outline-none bg-transparent placeholder-[#000000] placeholder-opacity-60"
-          onFocus={handleTypingInput}
-          onBlur={handleBlurInput}
-        />
-        {!isTypingInput && <Image src="/assets/img/admin/pencil.png" className="absolute top-[10px] right-[-5px]" alt={""} width={12} height={12} />}
-      </div>
-      <div className="relative">
-        <textarea
-          onChange={(e) => setLectureDescription(e.target.value)}
-          value={lectureDescription}
-          className="w-full max-w-[780px] h-auto resize-none rounded-lg px-2 pl-6 py-1 border border-1-[#D1D1D1] outline-none bg-transparent placeholder-[#000000] placeholder-opacity-60"
-          placeholder="ლექციის აღწერა"
-          onFocus={handleTypingTextarea}
-          onBlur={handleBlurTextarea}
-        ></textarea>
-        {!isTypingTextarea && <Image src="/assets/img/admin/pencil.png" className="absolute top-3 left-2" alt={""} width={12} height={12} />}
-      </div>
+  const handleDeleteTitleAndDescription = async () => {
+    if (!titleDescriptionData) return;
 
-      <button className="text-white bg-[#2FA8FF] py-1 px-7 rounded-lg w-36" onClick={handleSaveLecture}>
-        შენახვა
-      </button>
-    </div>
+    const { id } = titleDescriptionData;
+    try {
+      const response = await deleteTitleAndDescription(token, id);
+      if (response.message === "lecture content delete successfully") {
+        Swal.fire({
+          icon: "success",
+          title: response.message,
+          showConfirmButton: true,
+          timer: 1500,
+        });
+        setTitleDescriptionData(null);
+      } else {
+        console.error("An unexpected error occurred");
+        Swal.fire({
+          icon: "warning",
+          title: response.message,
+          showConfirmButton: false,
+          timer: 1500,
+        });
+      }
+    } catch (error) {
+      console.error("Error deleting video:", error);
+    }
+  };
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const data = await getLectureAndDescriptions(token, lectureId);
+        setTitleDescriptionData(data.lecture_content);
+      } catch (error) {
+        console.error("Error fetching categories:", error);
+      }
+    };
+
+    if (lectureId !== undefined && lectureId !== null) {
+      fetchData();
+    }
+  }, [lectureId]);
+
+  return (
+    <>
+      {titleDescriptionData ? (
+        <div className="relative flex flex-col gap-4">
+          <div className="w-48 relative">
+            <input
+              type="text"
+              value={titleDescriptionData?.title || ""}
+              placeholder="ლექციის სათაური"
+              onChange={(e) => setLectureTitle(e.target.value)}
+              className="h-auto resize-none rounded-lg pl-3 py-1 border border-1-[#D1D1D1] outline-none bg-transparent placeholder-[#000000] placeholder-opacity-60"
+              onFocus={handleTypingInput}
+              onBlur={handleBlurInput}
+            />
+            {!isTypingInput && <Image src="/assets/img/admin/pencil.png" className="absolute top-[10px] right-[-5px]" alt={""} width={12} height={12} />}
+          </div>
+          <div className="relative">
+            <textarea
+              onChange={(e) => setLectureDescription(e.target.value)}
+              value={titleDescriptionData?.description || ""}
+              className="w-full max-w-[780px] h-auto resize-none rounded-lg px-2 pl-6 py-1 border border-1-[#D1D1D1] outline-none bg-transparent placeholder-[#000000] placeholder-opacity-60"
+              placeholder="ლექციის აღწერა"
+              onFocus={handleTypingTextarea}
+              onBlur={handleBlurTextarea}
+            ></textarea>
+            {!isTypingTextarea && <Image src="/assets/img/admin/pencil.png" className="absolute top-3 left-2" alt={""} width={12} height={12} />}
+          </div>
+
+          <button className="text-white bg-red py-1 px-7 rounded-lg w-36" onClick={handleDeleteTitleAndDescription}>
+            წაშლა
+          </button>
+        </div>
+      ) : (
+        <div className="relative flex flex-col gap-4">
+          <div className="w-48 relative">
+            <input
+              type="text"
+              value={lectureTitle}
+              placeholder="ლექციის სათაური"
+              onChange={(e) => setLectureTitle(e.target.value)}
+              className="h-auto resize-none rounded-lg pl-3 py-1 border border-1-[#D1D1D1] outline-none bg-transparent placeholder-[#000000] placeholder-opacity-60"
+              onFocus={handleTypingInput}
+              onBlur={handleBlurInput}
+            />
+            {!isTypingInput && <Image src="/assets/img/admin/pencil.png" className="absolute top-[10px] right-[-5px]" alt={""} width={12} height={12} />}
+          </div>
+          <div className="relative">
+            <textarea
+              onChange={(e) => setLectureDescription(e.target.value)}
+              value={lectureDescription}
+              className="w-full max-w-[780px] h-auto resize-none rounded-lg px-2 pl-6 py-1 border border-1-[#D1D1D1] outline-none bg-transparent placeholder-[#000000] placeholder-opacity-60"
+              placeholder="ლექციის აღწერა"
+              onFocus={handleTypingTextarea}
+              onBlur={handleBlurTextarea}
+            ></textarea>
+            {!isTypingTextarea && <Image src="/assets/img/admin/pencil.png" className="absolute top-3 left-2" alt={""} width={12} height={12} />}
+          </div>
+
+          <button className="text-white bg-[#2FA8FF] py-1 px-7 rounded-lg w-36" onClick={handleSaveLecture}>
+            შენახვა
+          </button>
+        </div>
+      )}
+    </>
   );
 };
 
