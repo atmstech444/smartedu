b2d23206dab815feeff0730a35bf6d8a47a83df0
Tusho7 <sandrotushurashvili7@gmail.com>
2024-02-13 16:36:16 +0400

    style: change style

--- src/app/admin/edit-course/[id]/components/CourseDescription.tsx
+++ src/app/admin/edit-course/[id]/components/CourseDescription.tsx
@@ -2,48 +2,29 @@ import React from "react";
 
 const CourseDescription = ({ data, onFileChange }: any) => {
   return (
-    <div className="mt-6">
+    <div className="mt-6 w-[50%]">
       <div>
         <div className="flex flex-col gap-2">
-          <label
-            htmlFor="descriptionInput"
-            className="text-dark text-xl font-normal"
-          >
+          <label htmlFor="descriptionInput" className="text-dark text-xl font-normal">
             დაამატე კურსი:
           </label>
-          <input
-            id="courseTitle"
-            type="text"
-            defaultValue={data.title}
-            className="border border-[#DCDCDC] rounded-[30px] px-5 py-1 mt-1 text-start outline-none w-full"
-          />
+          <input id="courseTitle" type="text" defaultValue={data.title} className="border border-[#DCDCDC] rounded-[30px] px-5 py-1 mt-1 text-start outline-none w-full" />
         </div>
 
         <div className="flex flex-col gap-2 mt-9">
-          <label
-            htmlFor="descriptionInput"
-            className="text-dark text-xl font-normal"
-          >
+          <label htmlFor="descriptionInput" className="text-dark text-xl font-normal">
             კურსის აღწერა:
           </label>
-          <textarea
-            id="descriptionInput"
-            className="h-[280px] border border-[#DCDCDC] rounded-[32px] resize-none outline-none p-4 w-full"
-            defaultValue={data.description}
-          ></textarea>
+          <textarea id="descriptionInput" className="h-[280px] border border-[#DCDCDC] rounded-[32px] resize-none outline-none p-4 w-full" defaultValue={data.description}></textarea>
         </div>
 
         <div className="pt-9">
           <p className="text-dark text-xl font-normal">დაამატე კურსის ფოტო</p>
           <div className="flex flex-col mt-2 w-[30%] justify-center items-center">
-            <img
-              src={`https://smarteducation.shop/smarteducation_backend/public/${data.cover_image}`}
-              alt="Uploaded"
-              className="mt-10"
-            />
+            <img src={`https://smarteducation.shop/smarteducation_backend/public/${data.cover_image}`} alt="Uploaded" className="mt-10" />
             <input
               type="file"
-              className="w-[215px] mt-5"
+              className="w-[215px] mt-5 ml-20"
               onChange={(e) => {
                 const selectedFile = e.target?.files?.[0];
                 if (selectedFile) {
