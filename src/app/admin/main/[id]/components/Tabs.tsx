a4e1ac58aa8e98bec8f84fb7bb5178a2aeb5fc7f
Tusho7 <sandrotushurashvili7@gmail.com>
2024-02-13 16:01:43 +0400

    fix: fix build error

--- src/app/admin/main/[id]/components/Tabs.tsx
+++ src/app/admin/main/[id]/components/Tabs.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { Suspense } from "react";
 import LectureTitleAndDescription from "./LectureTitleAndDescription";
 
 const Tabs = ({ setActiveTab, activeTab }: any) => {
@@ -8,30 +8,31 @@ const Tabs = ({ setActiveTab, activeTab }: any) => {
 
   return (
     <>
-    <LectureTitleAndDescription />
-     <div className="flex gap-9 items-center border border-1-[#D1D1D1] p-1 w-[325px]">
-      <h1 className={`cursor-pointer ${activeTab === "წასაკითხი" ? "active" : ""}`} onClick={() => handleTabClick("წასაკითხი")}>
-        წასაკითხი
-      </h1>
-      <h1 className={`cursor-pointer ${activeTab === "ვიდეო" ? "active" : ""}`} onClick={() => handleTabClick("ვიდეო")}>
-        ვიდეო
-      </h1>
-      <h1 className={`cursor-pointer ${activeTab === "ქვიზი" ? "active" : ""}`} onClick={() => handleTabClick("ქვიზი")}>
-        ქვიზი
-      </h1>
-      <style jsx>{`
-        h1 {
-          padding: 5px 10px;
-        }
+      <Suspense>
+        <LectureTitleAndDescription />
+      </Suspense>
+      <div className="flex gap-9 items-center border border-1-[#D1D1D1] p-1 w-[325px]">
+        <h1 className={`cursor-pointer ${activeTab === "წასაკითხი" ? "active" : ""}`} onClick={() => handleTabClick("წასაკითხი")}>
+          წასაკითხი
+        </h1>
+        <h1 className={`cursor-pointer ${activeTab === "ვიდეო" ? "active" : ""}`} onClick={() => handleTabClick("ვიდეო")}>
+          ვიდეო
+        </h1>
+        <h1 className={`cursor-pointer ${activeTab === "ქვიზი" ? "active" : ""}`} onClick={() => handleTabClick("ქვიზი")}>
+          ქვიზი
+        </h1>
+        <style jsx>{`
+          h1 {
+            padding: 5px 10px;
+          }
 
-        .active {
-          background-color: #eee;
-          border-radius: 4px;
-        }
-      `}</style>
-    </div>
+          .active {
+            background-color: #eee;
+            border-radius: 4px;
+          }
+        `}</style>
+      </div>
     </>
-   
   );
 };
 
