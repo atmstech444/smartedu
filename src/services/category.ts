8af4d5df24dd5a0f969bb5219655cfe8bccf776c
Tusho7 <sandrotushurashvili7@gmail.com>
2024-02-20 12:38:07 +0400

    fix: fix url in category.ts

--- src/services/category.ts
+++ src/services/category.ts
@@ -1,5 +1,5 @@
 import axios from "axios";
-const url = "http://192.168.1.106:8000/api/";
+const url = "https://smarteducation.shop/smarteducation_backend/public/admin";
 import { parseCookies } from "nookies";
 
 export const storeCategory = async (data: any) => {
