d9e96bc0423867600f7ec3663a6ee0c52faf455e
teonateona12 <teona.piranishvili1@gmail.com>
2024-02-21 16:28:11 +0400

    fix: bugs

--- src/api/API_PATH.ts
+++ src/api/API_PATH.ts
@@ -1,2 +1,2 @@
-export const API_PATH = "http://192.168.1.106:8000/api/";
+export const API_PATH = "https://smarteducation.shop/smarteducation_backend/public/api/";
 export const API_STORAGE = "https://smarteducation.shop/smarteducation_backend/public/";
