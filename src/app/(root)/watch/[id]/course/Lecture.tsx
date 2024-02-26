5e73d8fb6ae84427408be2c68a38e995bed6030e
teonateona12 <teona.piranishvili1@gmail.com>
2024-02-26 13:39:24 +0400

    style: add loading

--- src/app/(root)/watch/[id]/course/Lecture.tsx
+++ src/app/(root)/watch/[id]/course/Lecture.tsx
@@ -82,46 +82,52 @@ const Lecture = (id: { id: any }) => {
           <Navigation id={id.id} />
         </UserMobileMenu>
       )}
-      <div className="flex gap-[24px] flex-col p-[24px] w-[90%]  bg-white rounded-md ">
-        <Image onClick={toggleMenuVisibility} src={Arrow} width={24} height={24} alt="image" className="lg:hidden mb-4" />
-        <h1 className="text-base font-bold text-black">ლექციის აღწერა</h1>
-        <p className="text-base	font-normal text-black">{lectureDetail?.lecture_content?.title}</p>
-        <p className="text-base font-bold	text-black">{lectureDetail?.lecture_name}</p>
-        {lectureDetail && lectureDetail.readings !== null ? (
-          <div className="flex gap-3 cursor-pointer" onClick={() => navigateToReading(lectureDetail.id)}>
-            <Image alt="book" src={Book} />
-            <div>
-              <p className=" m-0 font-medium text-black">მასალა</p>
-              <p className=" m-0">წასაკითხი</p>
-            </div>
-          </div>
-        ) : (
-          ""
-        )}
-
-        {lectureDetail &&
-          lectureDetail.videos !== null &&
-          lectureDetail &&
-          lectureDetail.videos.map((video, index) => (
-            <div className="flex gap-3 cursor-pointer" key={index} onClick={() => navigateToVideo(lectureDetail.id, video.id)}>
-              <Image alt="video" src={Video} />
+      {lectureDetail.course_id ? (
+        <div className="flex gap-[24px] flex-col p-[24px] w-[90%]  bg-white rounded-md ">
+          <Image onClick={toggleMenuVisibility} src={Arrow} width={24} height={24} alt="image" className="lg:hidden mb-4" />
+          <h1 className="text-base font-bold text-black">ლექციის აღწერა</h1>
+          <p className="text-base	font-normal text-black">{lectureDetail?.lecture_content?.title}</p>
+          <p className="text-base font-bold	text-black">{lectureDetail?.lecture_name}</p>
+          {lectureDetail && lectureDetail.readings !== null ? (
+            <div className="flex gap-3 cursor-pointer" onClick={() => navigateToReading(lectureDetail.id)}>
+              <Image alt="book" src={Book} />
               <div>
-                <p className=" m-0 font-medium	 text-black">{video.title}</p>
-                <p className=" m-0">ვიდეო</p>
+                <p className=" m-0 font-medium text-black">მასალა</p>
+                <p className=" m-0">წასაკითხი</p>
               </div>
             </div>
-          ))}
+          ) : (
+            ""
+          )}
 
-        {lectureDetail && lectureDetail.quizzes !== null && (
-          <div className="flex gap-3 cursor-pointer" onClick={() => navigateToQuiz(lectureDetail.id)}>
-            <Image alt="quizzes" src={Quizzes} />
-            <div>
-              <p className=" m-0 font-medium text-black">ლექციის ბოლოს</p>
-              <p className=" m-0">ქვიზი</p>
+          {lectureDetail &&
+            lectureDetail.videos !== null &&
+            lectureDetail &&
+            lectureDetail.videos.map((video, index) => (
+              <div className="flex gap-3 cursor-pointer" key={index} onClick={() => navigateToVideo(lectureDetail.id, video.id)}>
+                <Image alt="video" src={Video} />
+                <div>
+                  <p className=" m-0 font-medium	 text-black">{video.title}</p>
+                  <p className=" m-0">ვიდეო</p>
+                </div>
+              </div>
+            ))}
+
+          {lectureDetail && lectureDetail.quizzes !== null && (
+            <div className="flex gap-3 cursor-pointer" onClick={() => navigateToQuiz(lectureDetail.id)}>
+              <Image alt="quizzes" src={Quizzes} />
+              <div>
+                <p className=" m-0 font-medium text-black">ლექციის ბოლოს</p>
+                <p className=" m-0">ქვიზი</p>
+              </div>
             </div>
-          </div>
-        )}
-      </div>
+          )}
+        </div>
+      ) : (
+        <div className="flex gap-[24px] flex-col p-[24px] w-[90%]  bg-white rounded-md">
+          <h1 className="text-xl font-normal">იტვირთება...</h1>
+        </div>
+      )}
     </main>
   );
 };
