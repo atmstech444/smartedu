fc9eb92d17cb18754415523e1d7599e3c75f359b
Tusho7 <sandrotushurashvili7@gmail.com>
2024-03-01 16:33:24 +0400

    feat: add final quiz page with delete

---
+++ src/app/admin/final-quiz/services/deleteQuiz.ts
@@ -0,0 +1,16 @@
+import axios from "axios";
+
+export const deleteQuiz = async (token: any, quizId: any) => {
+  const config = {
+    headers: {
+      Authorization: `Bearer ${token}`,
+      "Content-Type": "application/json",
+    },
+  };
+  try {
+    const { data } = await axios.delete(`https://smarteducation.shop/smarteducation_backend/public/admin/course-lecture-final-quizzes/${quizId}`, config);
+    return data;
+  } catch (error) {
+    console.error(error);
+  }
+};
