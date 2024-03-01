d7528a7417a496d1833b46253a7cf072d80db789
Tusho7 <sandrotushurashvili7@gmail.com>
2024-03-01 16:21:38 +0400

    feat: add final quiz adding with score in quizupload

---
+++ src/app/admin/main/[id]/services/addFinalQuiz.ts
@@ -0,0 +1,21 @@
+import axios from "axios";
+
+export const addFinalQuiz = async (token: any, formdata: any, id: any) => {
+    const config = {
+      headers: {
+        Authorization: `Bearer ${token}`,
+        "Content-Type": "multipart/form-data",
+      },
+    };
+    try {
+      const { data } = await axios.post(
+        `https://smarteducation.shop/smarteducation_backend/public/admin/course-lecture-final-quizzes/${id}`,
+        formdata,
+        config
+      );
+      return data;
+    } catch (error) {
+      console.error(error);
+      throw error;
+    }
+  };
