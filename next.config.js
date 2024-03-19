883ba8b7e883438d2c5945550da3c522dded8b86
Tusho7 <sandrotushurashvili7@gmail.com>
2024-03-19 17:35:41 +0400

    chore: change url

--- next.config.js
+++ next.config.js
@@ -8,10 +8,10 @@ const nextConfig = {
     styledComponents: true,
   },
   env: {
-    // NEXT_ADMIN_API_URL: 'https://smarteducation.shop/smarteducation_backend/public/admin/',
-    // NEXT_ADMIN_PUBLIC_API_STORAGE: 'https://smarteducation.shop/smarteducation_backend/public/'
-    NEXT_ADMIN_API_URL: "http://192.168.1.108:8002/admin/",
-    NEXT_ADMIN_PUBLIC_API_STORAGE: "http://192.168.1.108:8002/",
+    NEXT_ADMIN_API_URL: 'https://smarteducation.shop/smarteducation_backend/public/admin/',
+    NEXT_ADMIN_PUBLIC_API_STORAGE: 'https://smarteducation.shop/smarteducation_backend/public/'
+    // NEXT_ADMIN_API_URL: "http://192.168.1.108:8002/admin/",
+    // NEXT_ADMIN_PUBLIC_API_STORAGE: "http://192.168.1.108:8002/",
   },
 };
 
