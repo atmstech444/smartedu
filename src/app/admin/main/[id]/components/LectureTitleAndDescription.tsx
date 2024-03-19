fe4d1583b7f565b8acabc77958fd363118a3826e
Tusho7 <sandrotushurashvili7@gmail.com>
2024-03-19 16:07:07 +0400

    chore: update lecture title and description component

--- src/app/admin/main/[id]/components/LectureTitleAndDescription.tsx
+++ src/app/admin/main/[id]/components/LectureTitleAndDescription.tsx
@@ -44,15 +44,15 @@ const LectureTitleAndDescription = () => {
   };
 
   const handleSaveLecture = async () => {
-    if (!lectureTitle.trim()) {
-      Swal.fire({
-        icon: "warning",
-        title: "გთხოვთ შეიყვანეთ ლექციის სათაური",
-        showConfirmButton: false,
-        timer: 1500,
-      });
-      return;
-    }
+    // if (!lectureTitle.trim()) {
+    //   Swal.fire({
+    //     icon: "warning",
+    //     title: "გთხოვთ შეიყვანეთ ლექციის სათაური",
+    //     showConfirmButton: false,
+    //     timer: 1500,
+    //   });
+    //   return;
+    // }
 
     if (!lectureDescription.trim()) {
       Swal.fire({
@@ -65,7 +65,7 @@ const LectureTitleAndDescription = () => {
     }
 
     const formData = new FormData();
-    formData.append("title", lectureTitle);
+    // formData.append("title", lectureTitle);
     formData.append("description", lectureDescription);
 
     try {
@@ -156,18 +156,18 @@ const LectureTitleAndDescription = () => {
         </div>
       ) : titleDescriptionData !== null ? (
         <div className="relative flex flex-col gap-4">
-          <div className="w-48 relative">
+          {/* <div className="w-full relative">
             <input
               type="text"
               value={titleDescriptionData.title || ""}
-              placeholder="ლექციის სათაური"
+              placeholder="ლექციის აღწერა"
               onChange={(e) => setLectureTitle(e.target.value)}
-              className="h-auto resize-none rounded-lg pl-3 py-1 border border-1-[#D1D1D1] outline-none bg-transparent placeholder-[#000000] placeholder-opacity-60"
+              className="w-[90%] h-auto resize-none rounded-lg pl-3 py-1 border border-1-[#D1D1D1] outline-none bg-transparent placeholder-[#000000] placeholder-opacity-60"
               onFocus={handleTypingInput}
               onBlur={handleBlurInput}
             />
             {!isTypingInput && <Image src="/assets/img/admin/pencil.png" className="absolute top-[10px] right-[-5px]" alt={""} width={12} height={12} />}
-          </div>
+          </div> */}
           <div className="relative">
             <textarea
               onChange={(e) => setLectureDescription(e.target.value)}
@@ -186,18 +186,18 @@ const LectureTitleAndDescription = () => {
         </div>
       ) : (
         <div className="relative flex flex-col gap-4">
-          <div className="w-48 relative">
+          {/* <div className=" relative">
             <input
               type="text"
               value={lectureTitle}
-              placeholder="ლექციის სათაური"
+              placeholder="ლექციის აღწერა"
               onChange={(e) => setLectureTitle(e.target.value)}
-              className="h-auto resize-none rounded-lg pl-3 py-1 border border-1-[#D1D1D1] outline-none bg-transparent placeholder-[#000000] placeholder-opacity-60"
+              className="w-full max-w-[780px] h-auto resize-none rounded-lg pl-6 py-1 border border-1-[#D1D1D1] outline-none bg-transparent placeholder-[#000000] placeholder-opacity-60"
               onFocus={handleTypingInput}
               onBlur={handleBlurInput}
             />
-            {!isTypingInput && <Image src="/assets/img/admin/pencil.png" className="absolute top-[10px] right-[-5px]" alt={""} width={12} height={12} />}
-          </div>
+            {!isTypingInput && <Image src="/assets/img/admin/pencil.png" className="absolute top-3 left-2" alt={""} width={12} height={12} />}
+          </div> */}
           <div className="relative">
             <textarea
               onChange={(e) => setLectureDescription(e.target.value)}
