452dfe94c2d3728cc04de47dbd1b96ac4032d921
Tusho7 <sandrotushurashvili7@gmail.com>
2024-03-19 12:10:50 +0400

    feat: create add certificate page

---
+++ src/app/admin/add-certificate/components/AddCertificate.tsx
@@ -0,0 +1,97 @@
+"use client";
+import React, { useState, useEffect } from "react";
+import Image from "next/image";
+import Swal from "sweetalert2";
+import { parseCookies } from "nookies";
+import { addCertificat } from "../services/addCertificate";
+import DeleteIcon from "@/public/assets/dynamic_icons/DeleteIcon";
+import { API_ADMIN_PATH, API_ADMIN_STORAGE } from "@/api/API_PATH";
+
+const AddCertificate = ({ courseId, courseData }: any) => {
+  const cookies = parseCookies();
+  const token = cookies.authToken;
+  console.log(courseId);
+  console.log(courseData);
+  const [uploadedImage, setUploadedImage] = useState<File | null>(null);
+  const [certificateImageUrl, setCertificateImageUrl] = useState<string | null>(null);
+
+  useEffect(() => {
+    if (courseData && courseData.certificate_image) {
+      setCertificateImageUrl(courseData.certificate_image);
+    }
+  }, [courseData]);
+
+  const handleFileUpload = (file: File | undefined) => {
+    if (file) {
+      setUploadedImage(file);
+    }
+  };
+
+  const handleDelete = () => {
+    setUploadedImage(null);
+  };
+
+  const addCertificate = async () => {
+    const formData = new FormData();
+    if (uploadedImage instanceof File) {
+      formData.append("certificate_image", uploadedImage);
+    }
+    formData.append("course_id", courseId);
+    try {
+      const response = await addCertificat(token, formData);
+      console.log(response);
+      const certificateImageUrl = response[0].certificate_image;
+      const decodedCertificateImageUrl = decodeURIComponent(certificateImageUrl);
+      console.log(decodedCertificateImageUrl);
+      if (response.message === "Certificate added successfully") {
+        setCertificateImageUrl(decodedCertificateImageUrl);
+        Swal.fire({
+          icon: "success",
+          title: response.message,
+          showConfirmButton: true,
+          timer: 1500,
+        });
+      } else {
+        console.error("Failed to add certificate");
+        Swal.fire({
+          icon: "warning",
+          title: response.message,
+          showConfirmButton: false,
+          timer: 1500,
+        });
+      }
+    } catch (error) {
+      console.error("An unexpected error occurred", error);
+    }
+  };
+  console.log(certificateImageUrl);
+  return (
+    <div>
+      {certificateImageUrl ? (
+        <div className="flex flex-col gap-12">
+          <Image src={API_ADMIN_STORAGE + certificateImageUrl} width={300} height={300} alt="Certificate" className="rounded-md" />
+          <button className="bg-red text-white py-1 w-[200px] px-2 rounded-full cursor-pointer" onClick={handleDelete}>
+            წაშლა
+          </button>
+        </div>
+      ) : uploadedImage ? (
+        <div className="flex flex-col gap-12">
+          <Image src={URL.createObjectURL(uploadedImage)} width={300} height={300} alt="Certificate" className="rounded-md" />
+          <button className="bg-red text-white w-[200px] px-2 py-1 rounded-full" onClick={handleDelete}>
+            წაშლა
+          </button>
+          <button className="bg-[#2FA8FF] text-white py-1 w-[200px] px-2 rounded-full cursor-pointer" onClick={addCertificate}>
+            შენხვა
+          </button>
+        </div>
+      ) : (
+        <label className="bg-[#2FA8FF] text-white py-[13px] px-2 rounded-lg cursor-pointer">
+          ატვირთე სერთიფიკატი
+          <input type="file" className="hidden" onChange={(e) => handleFileUpload(e.target.files?.[0])} />
+        </label>
+      )}
+    </div>
+  );
+};
+
+export default AddCertificate;
---
+++ src/app/admin/add-certificate/page.tsx
@@ -0,0 +1,61 @@
+"use client";
+import React, { Suspense, useEffect, useState } from "react";
+import Header from "@/components/Header";
+import Navbar from "../add-lecture/components/Navbar";
+import AddCertificate from "./components/AddCertificate";
+
+interface Lecture {
+  id: any;
+  name: any;
+}
+
+const useQueryParams = () => {
+  const [lectureId, setLectureId] = useState<string | undefined | null>(undefined);
+  const [lectures, setLectures] = useState<Lecture[]>([]);
+  const [courseData, setCourseData] = useState<any | null>(null);
+
+  useEffect(() => {
+    const searchParams = new URLSearchParams(window.location.search);
+    const id = searchParams.get("lectureId");
+    const lecturesParam = searchParams.get("lectures");
+    const courseDataParam = searchParams.get("courseData");
+
+    setLectureId(id);
+    if (lecturesParam) {
+      const lecturesArray: Lecture[] = JSON.parse(decodeURIComponent(lecturesParam));
+      setLectures(lecturesArray);
+    }
+
+    if (courseDataParam) {
+      const parsedCourseData = JSON.parse(decodeURIComponent(courseDataParam));
+      setCourseData(parsedCourseData);
+    }
+  }, []);
+
+  return { lectureId, lectures, courseData };
+};
+const Page = () => {
+  const { lectures, courseData } = useQueryParams();
+  return (
+    <div className="flex flex-col gap-8 w-[100%]">
+      <Header />
+      <div className="flex gap-8 w-[100%]">
+        <Navbar lectures={lectures} courseData={courseData} />
+
+        <div className="flex flex-col gap-10  mt-11 w-[97%]">
+          <AddCertificate courseId={courseData?.id} courseData={courseData} lectures={lectures} />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+const PageWrapper = () => {
+  return (
+    <Suspense fallback={<div>Loading...</div>}>
+      <Page />
+    </Suspense>
+  );
+};
+
+export default PageWrapper;
